736d0b7bf37a6f54c853e1bca55789eb1404a320
Saba Schock <sababorjsaba@gmail.com>
2019-01-23 12:51:13 -0800

    Nav

--- dog/src/componenet/NAvTabs.js
+++ dog/src/componenet/NAvTabs.js
@@ -6,39 +6,31 @@ function NavTabs() {
     <ul className="nav nav-tabs">
       <li className="nav-item">
         <Link to="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>
-          Home
+          Pupster
         </Link>
       </li>
       <li className="nav-item">
         <Link
-          to="/about"
-          className={window.location.pathname === "/about" ? "nav-link active" : "nav-link"}
+          to="/"
+          className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}
         >
           About
         </Link>
       </li>
       <li className="nav-item">
         <Link
-          to="/blog"
-          className={window.location.pathname === "/blog" ? "nav-link active" : "nav-link"}
+          to="/discover"
+          className={window.location.pathname === "/discover" ? "nav-link active" : "nav-link"}
         >
           Blog
         </Link>
       </li>
       <li className="nav-item">
         <Link
-          to="/contact"
-          className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+          to="/search"
+          className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
         >
           Contact
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/contact/learn"
-          className={window.location.pathname === "/contact/learn" ? "nav-link active" : "nav-link"}
-        >
-          Learn
         </Link>
       </li>
     </ul>
